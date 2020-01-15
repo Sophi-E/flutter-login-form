1cc881a458878a6894ee6177f4a44c01e3d59e4b
sophi-e <sophierithous@gmail.com>
2020-01-15 14:15:33 +0100

    added login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,16 +1,135 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget{
+class LoginScreen extends StatelessWidget {
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      body: Center(
-        child: Text(
-          'Login'
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: MediaQuery.of(context).size.height,
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 0.0),
+            margin: EdgeInsets.only(top: 32.0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Text(
+                          'login to your account',
+                          style: TextStyle(
+                              fontSize: 28.0,
+                              color: Color(0xff333333),
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 48.0),
+                        child: Text(
+                            'Enter your name, email address and create a password to sign up',
+                            style: TextStyle(
+                                fontSize: 18.0, color: Color(0xff767676))),
+                      ),
+                    ],
+                  ),
+                ),
+                // SizedBox(
+                //   height: 3.0,
+                // ),
+
+                SizedBox(
+                  height: 32.0,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Email Address',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        color: Color(0xff333333),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xff00a159), width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xff00a159), width: 0.8),
+                        ),
+                        hintText: 'Example@gmail.com',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 32.0,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Password',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18.0,
+                        color: Color(0xff333333),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xff00a159), width: 2.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Color(0xff00a159), width: 0.8),
+                        ),
+                        hintText: '*********',
+                      ),
+                      obscureText: true,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 32.0,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: Container(
+                    color: Color(0xffe5ed98),
+                    child: FlatButton(
+                      onPressed: null,
+                      child: Text(
+                        'LOGIN',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xff333333),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
-
 }
